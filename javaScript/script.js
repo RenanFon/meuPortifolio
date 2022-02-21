a0e93f471b4a874abbf7a7d4979a10e25e83ca58
Renan Fonseca <renanfonseca88@gmail.com>
2022-02-21 19:23:21 -0300

    javascript adicionado

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,9 @@
 
+const javaScript = document.querySelector('html')
+
+javaScript.classList.add('js')
+
+
 function initscroll(){
     const titulo = document.querySelector('h1')
 
@@ -24,3 +29,43 @@ function initscroll(){
 }
 
 initscroll()
+
+    function initaccordion(){
+
+    const accordionlist = document.querySelectorAll('.js-accordion h2')
+
+    
+
+    function activeAccordion(){
+    this.classList.toggle('ativo')
+    this.nextElementSibling.classList.toggle('ativo')
+
+    }
+
+
+
+    accordionlist.forEach((item)=>{
+        item.addEventListener('click',activeAccordion)
+    })
+}
+
+initaccordion()
+
+
+const sections = document.querySelectorAll('.js-scroll')
+
+const windowmetade = window.innerHeight * 0.6
+
+function animascroll(){
+
+    sections.forEach((section)=>{
+        const sectiontop = section.getBoundingClientRect().top - windowmetade
+        if(sectiontop < 0){
+            section.classList.add('ativo')
+        }
+    })
+}
+
+window.addEventListener('scroll',animascroll)
+
+animascroll()
